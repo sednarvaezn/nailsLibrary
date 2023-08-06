34c5e475f44dfc066e46a4b64c3c2c988ffee199
Sebastian Narvaez <sedan1220@hotmail.com>
2023-08-06 01:09:00 -0500

    Change next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["bucket.region.amazonaws.com"],
+    domains: ["nailslibrary.s3.amazonaws.com"],
   },
 };
 
